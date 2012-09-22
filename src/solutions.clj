bb9d806c51b84e1ce1738db435ad1e1eca249e3a
Gustav Veide <stiigviig@gmail.com>
2012-09-22 14:26:46 +0200

    Solve 69, implement merge-with

--- src/solutions.clj
+++ src/solutions.clj
@@ -309,3 +309,27 @@
   (= (my-count [[1 2] [3 4] [5 6]]) 3)
   (= (my-count '(13)) 1)
   (= (my-count '(:a :b :c)) 3))
+
+;;69
+;;Write a function which takes a function f and a variable number of maps. Your function should return a map that consists of the rest of the maps conj-ed onto the first. If a key occurs in more than one map, the mapping(s) from the latter (left-to-right) should be combined with the mapping in the result by calling (f val-in-result val-in-latter)
+
+(def my-merge-with
+  (fn [fun & maps]
+    (reduce
+      (fn [outer e]
+        (reduce
+          (fn [inner [k v]]
+            (update-in inner [k] #(if (nil? %) v (fun % v))))
+          outer e))
+      {} maps)
+    )
+  )
+
+(unit-test
+  "problem69"
+  (= (my-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+     {:a 4, :b 6, :c 20})
+  (= (my-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15})
+     {1 7, 2 10, 3 15})
+  (= (my-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+     {:a [3 4 5], :b [6 7], :c [8 9]}))
