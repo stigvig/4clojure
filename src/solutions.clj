94996c8ac3594b3c06133789f01554782ae27b22
Gustav Veide <stiigviig@gmail.com>
2012-09-23 21:01:58 +0200

    Solve 90, implement cartesian-product

--- src/solutions.clj
+++ src/solutions.clj
@@ -466,3 +466,20 @@
   (= (my-intersection #{0 1 2} #{3 4 5}) #{})
   (= (my-intersection #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d}))
 
+;;90
+;;Write a function which calculates the Cartesian product of two sets.
+
+(def cartesian-product
+  #(set (for [x %1 y %2] [x y]))
+  )
+
+(unit-test
+  "problem90"
+  (= (cartesian-product #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+     #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+       ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+       ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
+  (= (cartesian-product #{1 2 3} #{4 5})
+     #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+  (= 300 (count (cartesian-product (into #{} (range 10))
+                    (into #{} (range 30))))))
