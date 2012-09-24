7421379e8a963162931e6aada6c9c2358ec76b9c
Gustav Veide <stiigviig@gmail.com>
2012-09-24 20:41:03 +0200

    Solve 135, implement infix-math

--- src/solutions.clj
+++ src/solutions.clj
@@ -610,3 +610,27 @@
                        (fn [b]
                          (* a b))))
           5 5)))
+
+;;135
+;;Your friend Joe is always whining about Lisps using the prefix notation
+;;for math. Show him how you could easily write a function that does math
+;;using the infix notation. Is your favorite language that flexible, Joe?
+;;Write a function that accepts a variable length mathematical expression
+;;consisting of numbers and the operations +, -, *, and /. Assume a simple
+;;calculator that does not do precedence and instead just calculates left
+;;to right.
+
+(def my-infix-math
+  (fn [& args]
+    (loop [coll (rest args) acc (first args)]
+      (if (empty? coll)
+        acc
+        (recur (rest (rest coll)) ((first coll) acc (second coll))))))
+  )
+
+(unit-test
+  "problem135"
+  (= 7  (my-infix-math 2 + 5))
+  (= 42 (my-infix-math 38 + 48 - 2 / 2))
+  (= 8  (my-infix-math 10 / 2 - 1 * 2))
+  (= 72 (my-infix-math 20 / 2 + 2 + 4 + 8 - 6 - 10 * 9)))
