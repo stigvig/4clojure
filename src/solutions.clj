0c2dac9ac2cdf19cbff2afe96d35ba847232e653
Gustav Veide <stiigviig@gmail.com>
2012-09-28 20:49:16 +0200

    Added test for 91, is-connected?

--- src/solutions.clj
+++ src/solutions.clj
@@ -700,3 +700,24 @@
                            [:b :e] [:a :d] [:b :d] [:c :e]
                            [:d :e] [:c :f] [:d :f]]))
   (= false (my-visit-once? [[1 2] [2 3] [2 4] [2 5]])))
+
+;;91
+;;Given a graph, determine whether the graph is connected. A connected graph is such that a path exists between any two given nodes.
+;;-Your function must return true if the graph is connected and false otherwise.
+;;-You will be given a set of tuples representing the edges of a graph. Each member of a tuple being a vertex/node in the graph.
+;;-Each edge is undirected (can be traversed either direction).
+
+(def my-is-connected?)
+
+(unit-test
+  "problem91"
+  (= true (my-is-connected? #{[:a :a]}))
+  (= true (my-is-connected? #{[:a :b]}))
+  (= false (my-is-connected? #{[1 2] [2 3] [3 1]
+                               [4 5] [5 6] [6 4]}))
+  (= true (my-is-connected? #{[1 2] [2 3] [3 1]
+                              [4 5] [5 6] [6 4] [3 4]}))
+  (= false (my-is-connected? #{[:a :b] [:b :c] [:c :d]
+                               [:x :y] [:d :a] [:b :e]}))
+  (= true (my-is-connected? #{[:a :b] [:b :c] [:c :d]
+                              [:x :y] [:d :a] [:b :e] [:x :a]})))
